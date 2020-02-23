9d9135da43cae769b66fcbd173553f692f1bb0b9
Weidong Xu <weidongxu84@hotmail.com>
2020-02-23 14:13:31 +0800

    use async instead of sync for operation

---
+++ src/main/java/io/weidongxu/webapp/azmgmt/AzureResource.java
@@ -0,0 +1,11 @@
+package io.weidongxu.webapp.azmgmt;
+
+public class AzureResource {
+
+    public static String VM_RG = "vmess";
+    public static String VM_NAME = "vmess";
+
+    private AzureResource() {
+
+    }
+}
--- src/main/java/io/weidongxu/webapp/azmgmt/RestEndpoint.java
+++ src/main/java/io/weidongxu/webapp/azmgmt/RestEndpoint.java
@@ -1,13 +1,15 @@
 package io.weidongxu.webapp.azmgmt;
 
-import com.microsoft.azure.management.compute.PowerState;
 import com.microsoft.azure.management.compute.VirtualMachine;
+import com.microsoft.azure.management.compute.implementation.VirtualMachinesInner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import static io.weidongxu.webapp.azmgmt.AzureResource.*;
 
 @RestController
 public class RestEndpoint {
@@ -22,7 +24,8 @@ public class RestEndpoint {
     @ResponseBody
     String state(@RequestParam String secret) {
         checkAuthorization(secret);
-        VirtualMachine vm = azureManagement.getClient().virtualMachines().getByResourceGroup("vmess", "vmess");
+
+        VirtualMachine vm = azureManagement.getClient().virtualMachines().getByResourceGroup(VM_RG, VM_NAME);
         return vm.powerState().toString();
     }
 
@@ -33,20 +36,20 @@ public class RestEndpoint {
     @ResponseBody
     String power(@RequestParam String op, @RequestParam String secret) {
         checkAuthorization(secret);
-        VirtualMachine vm = azureManagement.getClient().virtualMachines().getByResourceGroup("vmess", "vmess");
+
+        VirtualMachinesInner clientVmInner = azureManagement.getClient().virtualMachines().manager().inner().virtualMachines()
         switch (op) {
             case "start":
-                vm.start();
+                clientVmInner.beginStart(VM_RG, VM_NAME);
                 break;
             case "stop":
-                vm.powerOff();
+                clientVmInner.beginPowerOff(VM_RG, VM_NAME);
                 break;
             case "restart":
-                vm.restart();
+                clientVmInner.beginRestart(VM_RG, VM_NAME);
                 break;
         }
-        vm.refresh();
-        return vm.powerState().toString();
+        return state(secret);
     }
 
     private void checkAuthorization(String secret) {
