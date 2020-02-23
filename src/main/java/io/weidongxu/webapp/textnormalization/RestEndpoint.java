faca19a7aed900cce1007ab116fcdb2e166f110f
Weidong Xu <weidongxu84@hotmail.com>
2020-02-23 12:31:02 +0800

    update

--- src/main/java/io/weidongxu/webapp/textnormalization/RestEndpoint.java
+++ src/main/java/io/weidongxu/webapp/textnormalization/RestEndpoint.java
@@ -23,7 +23,7 @@ public class RestEndpoint {
     @ResponseBody
     List<VirtualMachine> listVirtualMachines(@RequestParam String secret) {
         if (azureManagement.getSecret().equals(secret)) {
-            return azureManagement.getClient().virtualMachines().listByResourceGroup("appservice");
+            return azureManagement.getClient().virtualMachines().list();
         } else {
             throw new ForbiddenException();
         }
