0d79f50b6679264b85d47747ef58d338a2c980c2
Weidong Xu <weidongxu84@hotmail.com>
2020-02-23 23:36:17 +0800

    fix json object

--- src/main/java/io/weidongxu/webapp/azmgmt/VirtualMachineState.java
+++ src/main/java/io/weidongxu/webapp/azmgmt/VirtualMachineState.java
@@ -1,12 +1,14 @@
 package io.weidongxu.webapp.azmgmt;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.microsoft.azure.management.compute.PowerState;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Value;
 
 @Builder
-@Data
+@Value
 public class VirtualMachineState {
 
+    @JsonProperty("powerState")
     private final PowerState powerState;
 }
