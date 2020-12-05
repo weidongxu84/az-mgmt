96228ba74d5a211f43f3d56b6545200262b20560
Weidong Xu <weidongxu84@hotmail.com>
2020-12-05 09:42:25 +0800

    upgrade dependency

--- src/test/java/io/weidongxu/webapp/azmgmt/AzResourceManagementTests.java
+++ src/test/java/io/weidongxu/webapp/azmgmt/AzResourceManagementTests.java
@@ -1,13 +1,13 @@
 package io.weidongxu.webapp.azmgmt;
 
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@Ignore
-@RunWith(SpringRunner.class)
+@Disabled
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class AzResourceManagementTests {
 
