43927afcbe54dc57fd27af5bef432e2a5bf09937
Weidong Xu <weidongxu84@hotmail.com>
2021-03-27 13:41:13 +0800

    switch rg

--- src/main/java/io/weidongxu/webapp/azmgmt/AzureResource.java
+++ src/main/java/io/weidongxu/webapp/azmgmt/AzureResource.java
@@ -2,7 +2,7 @@ package io.weidongxu.webapp.azmgmt;
 
 public class AzureResource {
 
-    public static String VM_RG = "vmess";
+    public static String VM_RG = "vmess-ja";
     public static String VM_NAME = "vmess";
 
     private AzureResource() {
